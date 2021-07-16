a035b7574edb3e19d20d7e8ed6c9ce3c61b05516
BP <secret>
2021-07-16 21:45:31 +0200

    Adding people

---
+++ src/main/java/com/mkbewe/ayou/AyouApplication.java
@@ -0,0 +1,13 @@
+package com.mkbewe.ayou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyouApplication.class, args);
+	}
+
+}
